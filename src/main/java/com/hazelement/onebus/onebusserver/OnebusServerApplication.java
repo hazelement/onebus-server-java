f2dc1947ed4a55ce5601f0528d704f5d82cf4670
Harry Zheng <harry.zheng@outlook.com>
2020-01-22 20:29:06 -0800

    init commit

---
+++ src/main/java/com/hazelement/onebus/onebusserver/OnebusServerApplication.java
@@ -0,0 +1,13 @@
+package com.hazelement.onebus.onebusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnebusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnebusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazelement/onebus/onebusserver/OnebusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelement.onebus.onebusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnebusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
